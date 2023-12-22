abd2bd8a4331a6a59d251c6ccdc0a0c7ed7e3b8e
pcoloigner <philippe.coloigner@gmail.com>
2023-12-22 17:49:36 +0100

    Création du componsant 'sports'

--- frontend/planningsds-front-angular-app/src/app/app-routing.module.ts
+++ frontend/planningsds-front-angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SportsComponent } from './sports/sports.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : "sports", component : SportsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
