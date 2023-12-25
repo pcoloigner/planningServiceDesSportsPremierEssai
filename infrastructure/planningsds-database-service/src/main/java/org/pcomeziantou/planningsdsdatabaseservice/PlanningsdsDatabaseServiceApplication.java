51b27ed108d6bcf441951ceddd0e5a8202e4882c
pcoloigner <philippe.coloigner@gmail.com>
2023-12-25 12:34:16 +0100

    Ajout de template de service REST pour l'accès à la base de données

---
+++ infrastructure/planningsds-database-service/src/main/java/org/pcomeziantou/planningsdsdatabaseservice/PlanningsdsDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package org.pcomeziantou.planningsdsdatabaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningsdsDatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningsdsDatabaseServiceApplication.class, args);
+	}
+
+}
---
+++ infrastructure/planningsds-database-service/src/test/java/org/pcomeziantou/planningsdsdatabaseservice/PlanningsdsDatabaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pcomeziantou.planningsdsdatabaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningsdsDatabaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
